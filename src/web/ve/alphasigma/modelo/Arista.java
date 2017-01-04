c33ba61e3f8603074decb0526e324bfe3eb3e921
Aldrin Salazar <salazar.aldrin@gmail.com>
2017-01-04 13:41:03 -0400

    Documentacion del modelo terminada

--- src/web/ve/alphasigma/modelo/Arista.java
+++ src/web/ve/alphasigma/modelo/Arista.java
@@ -19,24 +19,34 @@
 package web.ve.alphasigma.modelo;
 
 /**
- * Arista representa una Arista en un campo de Grafos.
+ * Arista representa una Arista unidireccional en un campo de Grafos dirigidos.
  *
  * @see Conexion
  * @version 1.0.0 1/4/2017
  * @author Aldrin Salazar
  */
-public class Arista extends Conexion{ //TODO:Terminar Documentacion
+public class Arista extends Conexion{
+    /**
+     * En esta implementacion de una Arista, esta posee solamente una capacidad y un flujo.
+     */
+
+    /** Capacidad es el flujo maximo que puede circular por la Arista*/
     private int capacidad;
+
+    /** *Flujo es el flujo actual circulando en la Arista*/
     private int flujo;
 
-    public Arista(Vertice inicio, Vertice fin){
-        this(inicio, fin, 0, 0);
-    }
-
-    public Arista(Vertice inicio, Vertice fin, int capacidad){
-        this(inicio, fin, capacidad, 0);
-    }
-
+    /**
+     * Una Arista requiere tener un Vertice de origen, un Vertice de destino ademas de una capacidad y flujo positivos.
+     * La direccion esta en el sentido Inicio -> Fin:
+     *          G(V, A) => A(u, v) = u -> v
+     *
+     * @param inicio Vertice donde inicia la Arista.
+     * @param fin Vertice donde finaliza la Arista
+     * @param capacidad Flujo maximo que puede correr por la Arista.
+     * @param flujo Flujo actual que recorre la Arista.
+     * @throws IllegalArgumentException En caso de Flujo mayor a capacidad, Flujo negativo o Capacidad negativa.
+     */
     public Arista(Vertice inicio, Vertice fin, int capacidad, int flujo) throws IllegalArgumentException{
         super(inicio, fin);
 
@@ -51,31 +61,86 @@ public class Arista extends Conexion{ //TODO:Terminar Documentacion
         this.flujo = flujo;
     }
 
+    /**
+     * Abreviacion para una Arista con flujo actual igual a 0.
+     *
+     * @param inicio Vertice donde inicia la Arista.
+     * @param fin Vertice donde finaliza la Arista
+     * @param capacidad Flujo maximo que puede correr por la Arista.
+     */
+    public Arista(Vertice inicio, Vertice fin, int capacidad){
+        this(inicio, fin, capacidad, 0);
+    }
+
+    /**
+     * Abreviacion para una Arista con flujo actual igual a 0 y capacidad igual a 0.
+     *
+     * @param inicio Vertice donde inicia la Arista.
+     * @param fin Vertice donde finaliza la Arista
+     */
+    public Arista(Vertice inicio, Vertice fin){
+        this(inicio, fin, 0, 0);
+    }
+
+    /**
+     * Obtiene la Capacidad de la instancia.
+     *
+     * @return Capacidad de la instancia.
+     */
     public int getCapacidad() {
         return capacidad;
     }
 
+    /**
+     * Asigna la Capacidad de la instancia.
+     *
+     * @param capacidad Capacidad a asignar.
+     */
     public void setCapacidad(int capacidad) {
         this.capacidad = capacidad;
     }
 
+    /**
+     * Obtiene el Flujo de la instancia.
+     *
+     * @return Flujo de la instancia.
+     */
     public int getFlujo() {
         return flujo;
     }
 
+    /**
+     * Asigna un nuevo flujo que recorre la Arista, este debe cumplir con:
+     *      0 <= flujo <= capacidad
+     *
+     *  La asignacion de un flujo representa simbolicamente una Arista con Capacidad igual a capacidad-flujo pero
+     *  en sentido contrario, es decir, la asignacion de un flujo igual a la capacidad de la Arista resulta en el
+     *  cambio de sentido de esta, con la misma capacidad y un flujo 0.
+     *
+     * @param flujo Flujo nuevo a asignar.
+     * @throws IllegalArgumentException En caso de no cumplir con 0 <= flujo <= capacidad.
+     */
     public void setFlujo(int flujo) throws IllegalArgumentException{
         if(flujo > this.capacidad) {
             throw new IllegalArgumentException("Flujo mayor a capacidad");
         } else if(flujo == capacidad){
+            /** Al cumplirse implica la existencia de una Arista con capacidad 0, a fines de la implementacion, esto es
+            invertir el sentido de la Arista, conservando la capacidad pero con flujo 0.*/
             this.flujo = 0;
             invertir();
+        }else if(flujo < 0) {
+            throw new IllegalArgumentException("Flujo negativo");
         }else {
             this.flujo = flujo;
         }
     }
 
+    /**
+     * Obtiene el flujo restante que se puede asignar a la Arista, es decir, la diferencia entre Capacidad-Flujo.
+     *
+     * @return Diferencia entre Capacidad-Flujo.
+     */
     public int getFlujoRestante(){
         return this.capacidad - this.flujo;
     }
-
 }
--- src/web/ve/alphasigma/modelo/Modelo.java
+++ src/web/ve/alphasigma/modelo/Modelo.java
@@ -1,147 +1,146 @@
+/*
+ * Modelo.java       01/04/2017
+ * Copyright (C) 2017  Aldrin Salazar
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see http://www.gnu.org/licenses
+ *
+ */
 package web.ve.alphasigma.modelo;
 
-import java.util.*;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.Stack;
 import java.util.stream.Collectors;
 
-public class Modelo {//TODO:Documentacion
+/**
+ * Modelo es la representacion de un campo de Grafos dirigidos, en especifico una red donde cada Arista posee un flujo
+ * y una capacidad de flujo. Esta red debe poseer solamente un Vertice de tipo FUENTE y solamente un Vertice de tipo
+ * SUMIDERO para la aplicacion del algoritmo de Ford-Fulkerson.
+ *
+ * @see Arista
+ * @see Vertice
+ * @version 1.0.0 1/4/2017
+ * @author Aldrin Salazar
+ */
+public class Modelo {
+    /**
+     * El campo de Grafos dirigidos es par ordenado G(V, A), donde V es el conjunto de todos los Vertices, y A es el
+     * conjunto de todas las Aristas. En esta implementacion cada conjunto es guardado en una Lista
+     */
 
+    /** Lista de todos los vertices*/
     private List<Vertice> vertices;
+
+    /** Lista de todas las aristas*/
     private List<Arista> aristas;
 
+    /** Control al correr el algoritmo paso por paso*/
     private boolean corriendo;
 
+    /**
+     * El Modelo no requiere argumento alguno para su instanciacion.
+     */
     public Modelo() {
+        //Inicializa las listas donde se almacenan los vertces y aristas.
         limpiarGrafoPrincipal();
+        //El estado inicial es no corriendo.
         corriendo = false;
     }
 
-    public void añadirVertice(Vertice... v){
-        vertices.addAll(Arrays.asList(v));
-    }
-
-    public void añadirArista(Arista... e){
-        Arrays.asList(e).forEach(this::añadirArista);
-    }
-
-    public void añadirArista(Arista e) throws IllegalArgumentException{
-        if(validarArista(e)) {
-            aristas.add(e);
-        } else {
-            throw new IllegalArgumentException("Nodos que componen la arista no existen en el modelo");
-        }
-    }
-
-    public List<Vertice> getVertices() {
-        return vertices;
-    }
-
-    public List<Arista> getAristas() {
-        return aristas;
-    }
-
-    public int algoritmoFordFulkerson() throws IllegalStateException{
-        if(!redEsValida())
-            throw new IllegalStateException("Red invalida");
-
-        int flujoMaximo = 0;
-        Vertice origen = encontrarFuente();
-        Vertice sumidero = encontrarSumidero();
-        setAristasFlujoCero();
-
-        List<Vertice> caminoVertice;
-
-        while ((caminoVertice = existeCamino(origen, sumidero)) != null){
-            List<Arista> camino = caminoEntreVertices(caminoVertice);
-            int flujoMenor = camino.stream().mapToInt(Arista::getFlujoRestante).min().getAsInt();
-
-            camino.forEach((Arista e) -> e.setFlujo(e.getFlujo() + flujoMenor));
-            flujoMaximo += flujoMenor;
-        }
-
-        return flujoMaximo;
-    }
-
-    private boolean validarArista(Arista e){
-        return vertices.contains(e.getInicio()) && vertices.contains(e.getFin());
-    }
-
-    public void setAristasFlujoCero(){
-        aristas.forEach((Arista e) -> e.setFlujo(0));
-    }
-
-    public List<Arista> caminoEntreVertices(List<Vertice> camino){
-        ArrayList<Arista> resultado = new ArrayList<>();
-
-        for(int i = 0; i<camino.size()-1; i++){
-            final int j = i;
-            Optional<Arista> tmp = aristas.stream()
-                                    .filter((Arista e) -> e.getInicio() == camino.get(j))
-                                    .filter((Arista e) -> e.getFin() == camino.get(j+1))
-                                    .findFirst();
-
-            resultado.add(tmp.get());
-        }
-
-        return resultado;
-    }
-
-    public List<Vertice> existeCamino(Vertice origen, Vertice destino){
-        /**
-         * Implementacion del algoritmo Depth-first para recorrida de un grafo
-         */
-        ArrayList<Vertice> visitados = new ArrayList<>();
-        Stack<Vertice> control = new Stack<>();
-
-        Vertice actual = origen;
-
-        do {
-            visitados.add(actual);
-            control.add(actual);
-
-            Optional<Vertice> siguiente = adyacentes(actual).stream()
-                                                    .filter((Vertice e) -> !visitados.contains(e))
-                                                    .findFirst();
-
-            if(siguiente.isPresent()){
-                actual = siguiente.get();
-            }else {
-                control.pop();
-                if(control.empty()) break;
-                actual = control.pop();
-            }
-
-            if(actual.equals(destino)){
-                control.add(actual);
-                return new ArrayList<>(control);
-            }
-
-        }while (!actual.equals(destino));
-
-        return null;
-    }
-
-    public List<Vertice> adyacentes(Vertice v){
-        return aristas.stream()
-                .filter((Arista e) -> e.getInicio() == v)
-                .map(Arista::getFin)
-                .collect(Collectors.toList());
-    }
-
+    /**
+     * Limpia la lista de Vertices y Aristas del Modelo.
+     */
     private void limpiarGrafoPrincipal(){
         vertices = new ArrayList<>();
         aristas = new ArrayList<>();
     }
 
-    public boolean redEsValida(){
-        Vertice o = encontrarFuente();
-        Vertice s = encontrarSumidero();
-
-        if(o == null || s == null)
-            return false;
-
-        return existeCamino(o, s) != null;
+    /**
+     * Añade los Vertices al modelo.
+     *
+     * @param v Array con los vertices a añadir, en caso de ser varios.
+     */
+    public void añadirVertice(Vertice... v){
+        vertices.addAll(Arrays.asList(v));
     }
 
+    /**
+     * Añade las Aristas al modelo. Cada arista es añadida individualmente para su validacion.
+     *
+     * @param e Array con las aristas a añadir, en caso de ser varias.
+     */
+    public void añadirArista(Arista... e){
+        Arrays.asList(e).forEach(this::añadirArista);
+    }
+
+    /**
+     * Añade una Arista al modelo, ambos Vertices de la Arista deben pertenecer al Modelo.
+     *
+     * @param e Arista a añadir.
+     * @throws IllegalArgumentException En caso de que los Vertices que componen la Arista no pertenezcan al Modelo.
+     */
+    public void añadirArista(Arista e) throws IllegalArgumentException{
+        if(validarArista(e)) {
+            aristas.add(e);
+        } else {
+            throw new IllegalArgumentException("Vertices que componen la arista no existen en el modelo.");
+        }
+    }
+
+    /**
+     * Obtiene la Lista de Vertices del Modelo.
+     *
+     * @return Lista con los Vertices del Modelo.
+     */
+    public List<Vertice> getVertices() {
+        return vertices;
+    }
+
+    /**
+     * Obtiene la Lista de Aristas del Modelo.
+     *
+     * @return Lista con las Aristas del Modelo.
+     */
+    public List<Arista> getAristas() {
+        return aristas;
+    }
+
+    /**
+     * Valida que una Arista posea ambos Vertices en el Modelo.
+     *
+     * @param e Arista a validar
+     * @return Si ambos Vertices estan o no en el modelo.
+     */
+    private boolean validarArista(Arista e){
+        return vertices.contains(e.getInicio()) && vertices.contains(e.getFin());
+    }
+
+    /**
+     * Asigna a 0 el flujo de todas las Aristas del Modelo.
+     */
+    public void setAristasFlujoCero(){
+        aristas.forEach((Arista e) -> e.setFlujo(0));
+    }
+
+    /**
+     * Busca en la lista de Vertices alguno de tipo FUENTE.
+     *
+     * @see Vertice
+     * @return Instancia de Vertice de tipo FUENTE encontrado, o null de no existir.
+     */
     public Vertice encontrarFuente(){
         Optional<Vertice> tmp = vertices.stream()
                 .filter((Vertice e) -> e.getTipo() == Vertice.Tipo.FUENTE)
@@ -150,6 +149,12 @@ public class Modelo {//TODO:Documentacion
         return tmp.isPresent() ? tmp.get() : null;
     }
 
+    /**
+     * Busca en la lista de Vertices alguno de tipo SUMIDERO.
+     *
+     * @see Vertice
+     * @return Instancia de Vertice de tipo SUMIDERO encontrado, o null de no existir.
+     */
     public Vertice encontrarSumidero(){
         Optional<Vertice> tmp = vertices.stream()
                 .filter((Vertice e) -> e.getTipo() == Vertice.Tipo.SUMIDERO)
@@ -158,4 +163,142 @@ public class Modelo {//TODO:Documentacion
         return tmp.isPresent() ? tmp.get() : null;
     }
 
+    /**
+     * Obtiene la lista de Vertices adyacentes a otro, esto es, todos los Vertices que son inmediatamente accesibles
+     * desde el.
+     *
+     * @param v Vertice a consultar
+     * @return Lista con los Vertices adyacentes. En caso de no tener adyacentes sera una lista vacia.
+     */
+    public List<Vertice> adyacentes(Vertice v){
+        return aristas.stream()                           //Para cada arista ..
+                .filter((Arista e) -> e.getInicio() == v) //.. todas las Aristas que inician en v ..
+                .map(Arista::getFin)                      //.. todos los Vertices que son el final de estas Aristas ..
+                .collect(Collectors.toList());            //.. se convierte a una lista
+    }
+
+    /**
+     * Valida si una red cumple con las condiciones para calcular el flujo maximo:
+     * -Poseer un Vertice FUENTE
+     * -Poseer un Vertice SUMIDERO
+     * -Existe un camino desde FUENTE a SUMIDERO
+     *
+     * @return Si la red cumple las condiciones o no.
+     */
+    public boolean redEsValida(){
+        Vertice o = encontrarFuente();
+        Vertice s = encontrarSumidero();
+
+        if(o == null || s == null)                       //Si falta la fuente o el sumidero ..
+            return false;                                //.. es una red invalida.
+
+        return existeCamino(o, s) != null;               //Si existe camino entre fuente y sumidero.
+    }
+
+    /**
+     * Busca un camino entre dos Vertices, desde el origen al destino, el orden es importante al ser un grafo dirigido,
+     * usa el algoritmo de recorrida Depth-First para la busqueda en el grafo.
+     *
+     * @param origen Vertice de donde inicia la busqueda.
+     * @param destino Vertice objetivo a llegar.
+     * @return Lista con los Vertices que componen el camino ordenados en el orden a visitar, o null de no existir camino.
+     */
+    public List<Vertice> existeCamino(Vertice origen, Vertice destino){
+        /**
+         * Implementacion del algoritmo Depth-first para recorrida de un Grafo.
+         */
+
+        //Se necesita marcar cada nodo ya visitado
+        ArrayList<Vertice> visitados = new ArrayList<>();
+        //Se guarda el orden en que recorremos, para hacer backtracking y para obtener el recorrido
+        Stack<Vertice> control = new Stack<>();
+
+        //Inicia en el origen
+        Vertice actual = origen;
+
+        do {
+            visitados.add(actual);                                      //.. se marca el actual como visitado ..
+            control.add(actual);                                        //.. se añade el actual al camino tentativo ..
+
+            Optional<Vertice> siguiente = adyacentes(actual).stream()   //Para cada Vertice adyacente al actual ..
+                    .filter((Vertice e) -> !visitados.contains(e))      //.. todos los Vertices no visitados ..
+                    .findFirst();                                       //.. se toma el primero.
+
+            if(siguiente.isPresent()){                                  //Si existe un adyacente sin visitar ..
+                actual = siguiente.get();                               //.. es el siguiente a iterar.
+            }else {                                                     //De lo contrario ..
+                control.pop();                                          //Se quita el tope de la pila de camino tentativo ..
+                if(control.empty()) return null;                        //.. si la pila esta vacia, no hay camino posible ..
+                actual = control.pop();                                 //De lo contrario, se desapila el ultimo, y se itera.
+            }
+
+            if(actual.equals(destino)){                                 //Si el Vertice donde se esta es el buscado ..
+                control.add(actual);                                    //.. se añade a el camino ..
+                return new ArrayList<>(control);                        //.. se convierte a lista.
+            }
+
+        }while (!actual.equals(destino));
+
+        return null;
+    }
+
+    /**
+     * Dado una lista de Vertices, en orden a recorrer, esta funcion da las Aristas, en orden a recorrer, que deben ser
+     * tomadas para llegar desde el primero en la lista al ultimo. Asume un camino posible entre ellos.
+     *
+     * @param camino Lista de Vertices en orden, a buscar las Aristas a recorrer.
+     * @return Lista con las Aristas en orden para llegar desde el primero al ultimo.
+     */
+    public List<Arista> caminoEntreVertices(List<Vertice> camino){
+        ArrayList<Arista> resultado = new ArrayList<>();
+
+        for(int i = 0; i<camino.size()-1; i++){
+            final int j = i;
+            Optional<Arista> tmp = aristas.stream()                         //Para cada Arista ..
+                    .filter((Arista e) -> e.getInicio() == camino.get(j))   //.. que inicia en el actual ..
+                    .filter((Arista e) -> e.getFin() == camino.get(j+1))    //.. y termina en el siguiente ..
+                    .findFirst();                                           //.. se toma el primero (Unico).
+
+            resultado.add(tmp.get());                                       //Se añade al camino.
+        }
+
+        return resultado;
+    }
+
+    /**
+     * Implementacion del algoritmo Ford-Fulkerson para determinar el flujo maximo de una red de Grafos dirigidos.
+     *
+     * @return El flujo maximo entre la fuente y el sumidero de la red.
+     * @throws IllegalStateException En caso de no ser valida la red.
+     */
+    public int algoritmoFordFulkerson() throws IllegalStateException{
+        /**
+         * Algoritmo Ford-Fulkerson, con busqueda Depth-First.
+         */
+
+        //La red debe ser valida
+        if(!redEsValida())
+            throw new IllegalStateException("Red invalida");
+
+        //Flujo maximo de la red
+        int flujoMaximo = 0;
+        Vertice origen = encontrarFuente();
+        Vertice sumidero = encontrarSumidero();
+        //Camino desde Origen a Sumidero
+        List<Vertice> caminoVertice;
+
+        setAristasFlujoCero();                                              //1- Establecer a 0 el flujo de todas las aristas
+        while ((caminoVertice = existeCamino(origen, sumidero)) != null){   //2- Mientras exista un camino entre el Origen y el Sumidero
+            List<Arista> camino = caminoEntreVertices(caminoVertice);
+            int flujoMenor = camino.stream()                                //3- Para cada Arista del camino ...
+                    .mapToInt(Arista::getFlujoRestante)                     //   .. se toma el flujo que puede aumentar ..
+                    .min()                                                  //   .. el menor flujo ..
+                    .getAsInt();                                            //   .. se guarda.
+
+            camino.forEach((Arista e) -> e.setFlujo(e.getFlujo() + flujoMenor)); //4- Se aumenta el flujo en cada Arista del camino en esa cantidad
+            flujoMaximo += flujoMenor;                                           //5- Esta cantidad se suma al flujo maximo.
+        }
+
+        return flujoMaximo;
+    }
 }
